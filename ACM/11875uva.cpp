dd3c4e95cbca0187a9ad72df6a40629d5340e4d6
root <root@localhost.localdomain>
2014-07-31 00:37:48 +0300

    2

--- ACM/11875uva.cpp
+++ ACM/11875uva.cpp
@@ -1,1 +1,37 @@
-
+//#include <iostream>
+//#include <cmath>
+//#include <math.h>
+//#include <string>
+//#include <sstream>
+//#include <algorithm>
+//#include <cstring>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <vector>
+//using namespace std;
+//typedef unsigned long long ull;
+//
+//
+//int
+//main()
+//{
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+//    int cases;
+//    int i=0;
+//    cin>>cases;
+//    while(cases--)
+//    {
+//        i++;
+//        int n;
+//        cin>>n;
+//        vector<int> kids(n);
+//        for(int i=0; i<n; i++)
+//            cin>>kids[i];
+//        sort(kids.begin(),kids.end());
+//        cout<<"Case "<<i<<": "<<kids[n/2]<<endl;
+//    }
+//    return 0;
+//}
+//
+//
---
+++ ACM/build/Debug/GNU-Linux-x86/11875uva.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux-x86/11875uva.o: 11875uva.cpp
